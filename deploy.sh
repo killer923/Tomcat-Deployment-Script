af4610308148cd8b054db3671b44afceb39d0e11
Abhishek Bhatia <killer923@users.noreply.github.com>
2016-09-25 04:26:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
+debug=false
+while getopts ":d" opt; do
+  case $opt in
+    d)
+      debug=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 control_c(){
 	tomcatPid=`ps -ef | grep org.apache.catalina.startup.Bootstrap\ start |  grep -v "grep"| awk '{ printf $2 }'`
 	sh shutdown.sh
@@ -8,8 +20,15 @@ control_c(){
 
 trap control_c SIGINT
 
-export JPDA_ADDRESS=8000
-export JPDA_TRANSPORT=dt_socket
 
-sh catalina.sh jpda start
+if $debug; then
+	echo 'Starting in Debug Mode...'
+	export JPDA_ADDRESS=8000
+	export JPDA_TRANSPORT=dt_socket
+	sh catalina.sh jpda start
+else
+	echo 'Starting normally...'
+	sh catalina.sh start
+fi
+
 tail -f ../logs/catalina.out
